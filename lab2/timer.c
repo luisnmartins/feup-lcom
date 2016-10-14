8e305f1dd5a13d614b361bb0193da245ed731b8d
Luís Martins <luis.n.martins97@gmail.com>
2016-10-14 11:12:04 +0000

    Last update lab2 part 2

--- lab2/timer.c
+++ lab2/timer.c
@@ -3,13 +3,7 @@
 #include <minix/drivers.h>
 #include <minix/com.h>
 
-#define RBC_COUNT0 0xE2
-#define RBC_COUNT1 0xE4
-#define RBC_COUNT2 0xE8
-#define COPY_4LAST 0x0F
-#define COUNTER0_LSBMSB_CWORD 0x3f
-#define COUNTER1_LSBMSB_CWORD 0x7f
-#define COUNTER2_LSBMSB_CWORD 0xbf
+
 
 unsigned int contador = 0;
 unsigned int hook_id = T0_IRQSET;
@@ -17,9 +11,11 @@ unsigned int hook_id = T0_IRQSET;
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
+
 	unsigned long final;
 	unsigned long conf;
 	unsigned char div_lsb, div_msb;
+	unsigned char timer_select, control_4first;
 	if(timer_get_conf(timer, &conf) != 0)
 	{
 		return 1;
@@ -29,50 +25,38 @@ int timer_set_square(unsigned long timer, unsigned long freq) {
 	div_lsb = div;
 	div_msb = div >> 8;
 	final = conf & COPY_4LAST;
-	if (timer == 0)
+	if(timer ==0)
 	{
-		final &= COUNTER0_LSBMSB_CWORD;
-		if(sys_outb(TIMER_CTRL, final) == OK &&
-		sys_outb(TIMER_0, div_lsb)== OK &&
-		sys_outb(TIMER_0, div_msb) == OK)
+		control_4first = COUNTER0_LSBMSB_CWORD;
+		timer_select = TIMER_0;
+	}
+	else if(timer == 1)
+	{
+		control_4first = COUNTER1_LSBMSB_CWORD;
+		timer_select = TIMER_1;
+
+	}
+	else if(timer == 2)
+	{
+		control_4first = COUNTER2_LSBMSB_CWORD;
+		timer_select = TIMER_2;
+	}
+
+		final &= control_4first;
+		if(sys_outb(TIMER_CTRL, final) != OK)
 		{
+			printf("Can't pass the control word to the TIMER CONTROL");
+			return 1;
+		}
+		if(sys_outb(timer_select, div_lsb) != OK || sys_outb(timer_select, div_msb) != OK)
+		{
+			printf("Can't pass the config to the TIMER");
+			return 1;
+		}
+
 			printf("Time set with success!!\n");
 			return 0;
-		}
-		else
-			{
-			printf ("Something went wrong!\n");
-			return 1;
-			}
-	}
-	else if (timer == 1)
-	{
-		final = final & COUNTER1_LSBMSB_CWORD;
-		if(sys_outb(TIMER_CTRL, final) == OK &&
-		sys_outb(TIMER_1, div_lsb) == OK &&
-		sys_outb(TIMER_1, div_msb) == OK)
-		{printf("Time set with success!!\n");
-		return 0;}
-		else {
-			printf ("Something went wrong!\n");
-			return 1;
-		}
 
-
-	}
-	else if (timer == 2) {
-		final = final & COUNTER2_LSBMSB_CWORD;
-		if(sys_outb(TIMER_CTRL, final) == OK &&
-		sys_outb(TIMER_2, div_lsb) == OK &&
-		sys_outb(TIMER_2, div_msb) == OK)
-			{printf("Time set with success!!\n");
-			return 0;}
-		else {
-			printf ("Something went wrong!\n");
-			return 1;
-		}
-
-	}
 
 	return 1;
 
@@ -83,9 +67,16 @@ int timer_subscribe_int() {
 
 
 	if(sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id) != OK)
+	{
+		printf("Can't send and define the policy");
 		return -1;
-	if(!sys_irqenable(&hook_id) != OK)
+	}
+
+	if(sys_irqenable(&hook_id) != OK)
+	{
+		printf("Can't enable the subscription");
 		return -1;
+	}
 
 	return BIT(T0_IRQSET);
 
@@ -121,36 +112,47 @@ void timer_int_handler() {
 int timer_get_conf(unsigned long timer, unsigned char *st) {
 
 	unsigned long read_info;
+	unsigned char rbc_count, timer_select;
+
 	if (timer == 0) {
-		printf("word : 0x%x\n", RBC_COUNT0);
-		if (sys_outb(TIMER_CTRL, RBC_COUNT0) == 0
-				&& sys_inb(TIMER_0, &read_info) == 0)
-			*st = (unsigned char) read_info;
+		rbc_count = RBC_COUNT0;
+		timer_select = TIMER_0;
+	}
+	else if(timer == 1)
+	{
+		rbc_count = RBC_COUNT1;
+		timer_select = TIMER_1;
+	}
+	else if(timer == 2)
+	{
+		rbc_count = RBC_COUNT2;
+		timer_select = TIMER_2;
+	}
+
+
+		printf("word : 0x%x\n", rbc_count);
+		if (sys_outb(TIMER_CTRL, rbc_count) != 0)
+		{
+			printf("Can't pass the control word to the TIMER CONTROL");
+			return 1;
+		}
+		if(sys_inb(timer_select, &read_info) != 0)
+		{
+			printf("Can't get the configuration of TIMER");
+			return 1;
+		}
+		*st = (unsigned char) read_info;
 		return 0;
 
-	}
-	if (timer == 1) {
-		printf("word : 0x%x\n", RBC_COUNT1);
-		if (sys_outb(TIMER_CTRL, RBC_COUNT1) == 0
-				&& sys_inb(TIMER_1, &read_info) == 0)
-			*st = (unsigned char) read_info;
-		return 0;
-	}
-	if (timer == 2) {
-		if (sys_outb(TIMER_CTRL, RBC_COUNT2) == 0
-				&& sys_inb(TIMER_2, &read_info) == 0)
-			*st = (unsigned char) read_info;
-		return 0;
-	}
-	return 1;
 }
 
 int timer_display_conf(unsigned char conf) {
 
 	int bcd = 0, null = 0, output = 0;
+	unsigned char program_mode;
 
 	printf("configuration: 0x%x\n", conf);
-	if (conf & BIT(0)) {
+	if (conf & TIMER_BCD) {
 		bcd = 1;
 	}
 	if (conf & BIT(6)) {
@@ -162,65 +164,92 @@ int timer_display_conf(unsigned char conf) {
 	printf("BCD = %d\n", bcd);
 	printf("Null Count = %d\n", null);
 	printf("Output = %d\n", output);
-	if (conf & BIT(3)) {
-		if (conf & BIT(2)) {
-			if (conf & BIT(1)) {
-				printf("Programmed Mode = SQUARE WAVE MODE\n");
-
-			} else {
-				printf("Programmed Mode = RATE GENERATOR\n");
-
+	program_mode = conf >> 1;
+	program_mode &= COPY_3LAST;
+	printf("PROGRAM MODE: ");
+	switch(program_mode)
+	{
+		case 0:
+			{
+				printf("INTERRUPT ON TERMINAL COUNT\n");
+				break;
 			}
-		} else {
-			if (conf & BIT(1)) {
-				printf(
-						"Programmed Mode = HARDWARE TRIGGERED STROBE (RETRIGGERABLE)\n");
-
-			} else {
-				printf("Programmed Mode = SOFTWARE TRIGGERED STROBE\n");
-
+		case 1:
+			{
+				printf("HARDWARE RETRIGGERABLE ONE-SHOT\n");
+				break;
 			}
+		case 2:
+		{
+			printf("RATE GENERATOR\n");
+			break;
 		}
-	} else {
-		if (conf & BIT(2)) {
-			if (conf & BIT(1)) {
-				printf("Programmed Mode = SQUARE WAVE MODE\n");
-
-			} else {
-				printf("Programmed Mode = RATE GENERATOR\n");
-
-			}
-		} else {
-			if (conf & BIT(1)) {
-				printf("Programmed Mode = HARDWARE RETRIGGERABLE ONE-SHOT\n");
-			} else {
-				printf("Programmed Mode = INTERRUPT ON TERMINAL COUNT\n");
-			}
+		case 3:
+		{
+			printf("SQUARE WAVE MODE\n");
+						break;
+		}
+		case 4:
+		{
+			printf("SOFTWARE TRIGGERED STROBE\n");
+						break;
+		}
+		case 5:
+		{
+			printf("HARDWARE TRIGGERED STROBE (RETRIGGERABLE)\n");
+						break;
+		}
+		case 6:
+		{
+			printf("RATE GENERATOR\n");
+						break;
+		}
+		case 7:
+		{
+			printf("SQUARE WAVE MODE\n");
+						break;
+		}
+		defalut:
+		{
+			printf("No mode available\n");
+			break;
 		}
 	}
 
-	if (conf & BIT(5)) {
-		if (conf & BIT(4)) {
+
+
+	if(conf & TIMER_LSB_MSB)
+	{
 			printf("Type of Access = LSB followed by MSB\n");
 			return 0;
-		} else {
+	} else if(conf & TIMER_MSB)
+	{
 			printf("Type of Access = MSB\n");
 			return 0;
-		}
-	} else {
-		if (conf & BIT(4)) {
+	}
+	else if(conf & TIMER_LSB)
+	{
 			printf("Type of Access = LSB\n");
 			return 0;
-		} else
-			return 1;
 	}
+
+	return 1;
+
 
 }
 
 int timer_test_square(unsigned long freq) {
 
+	if(freq <= 0)
+	{
+		printf("Invalid Frequency\n");
+		return 1;
+	}
 	if (timer_set_square(TIMER_SEL0, freq) == 0)
+	{
+
 		return 0;
+	}
 	else
 		return 1;
 }
@@ -229,8 +258,12 @@ int timer_test_int(unsigned long time) {
 	int ipc_status, irq_set=timer_subscribe_int();
 	message msg;
 	int r;
-
-	while (contador < (time*60))
+	if(time <= 0)
+	{
+		printf("Time not valid!");
+		return 1;
+	}
+	while (contador <= (time*60))
 	{ /* You may want to use a different condition */
 		/* Get a request message. */
 
@@ -245,10 +278,10 @@ int timer_test_int(unsigned long time) {
 
 				if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
 
-					if (contador % 60 == 0)
+					if (contador % 60 == 0 && contador>=60)
 					{
 
-						printf("\nMessage");
+						printf("\nMessage on %d seconds", contador/60);
 
 
 					}
@@ -279,6 +312,12 @@ int timer_test_int(unsigned long time) {
 }
 int timer_test_config(unsigned long timer) {
 
+
+	if(timer != 0 && timer != 1 && timer != 2)
+	{
+		printf("Timer not valid. Please Try Again!!\n");
+		return 1;
+	}
 	unsigned char var;
 	if (timer_get_conf(timer, &var) == 0) {
 		if (timer_display_conf(var) == 0)

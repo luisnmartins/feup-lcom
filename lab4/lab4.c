7d66f9565d679d0d52e696e3b7b62ff5d65399bf
up201503344 <up201503344@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-11-11 09:47:59 +0000

    update

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -8,6 +8,7 @@
 
 static int proc_args(int argc, char **argv);
 static unsigned long parse_ulong(char *str, int base);
+static long parse_long(char *str, int base);
 static void print_usage(char **argv);
 
 int main(int argc, char **argv)
@@ -75,10 +76,10 @@ static int proc_args(int argc, char **argv)
 			printf("MOUSE: wrong no. of arguments for test_gesture()\n");
 			return 1;
 		}
-		leng = parse_ulong(argv[2], 10);
-		if (leng == ULONG_MAX)
+		leng = parse_long(argv[2], 10);
+		if (leng == LONG_MAX)
 					return 1;
-		printf("test4::test_gesture(%lu)", leng);
+		printf("test4::test_gesture(%i)", leng);
 
 		return test_gesture(leng);
 	}
@@ -110,4 +111,27 @@ static unsigned long parse_ulong(char *str, int base)
 
 	/* Successful conversion */
 	return val;
+}
+
+static long parse_long(char *str, int base)
+{
+		char *endptr;
+		unsigned long val;
+
+		/* Convert string to unsigned long */
+		val = strtoul(str, &endptr, base);
+
+		/* Check for conversion errors */
+		if ((errno == ERANGE && val == LONG_MAX) || (errno != 0 && val == 0)) {
+			perror("strtoul");
+			return LONG_MAX;
+		}
+
+		if (endptr == str) {
+			printf("timer: parse_ulong: no digits were found in %s\n", str);
+			return LONG_MAX;
+		}
+
+		/* Successful conversion */
+		return val;
 }

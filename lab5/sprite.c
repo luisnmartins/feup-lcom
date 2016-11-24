80d3064234fb6a1507b03fe6f346e1f32dc6d4d2
up201504749 <up201504749@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-11-24 16:53:40 +0000

    ashda

---
+++ lab5/sprite.c
@@ -0,0 +1,45 @@
+#include "sprite.h"
+
+/**
+ Creates a new sprite with pixmap "pic", with specified
+ *
+ position (within the screen limits) and speed;
+ *
+ Does not draw the sprite on the screen
+ *
+ Returns NULL on invalid pixmap.
+ */
+Sprite *create_sprite(char * pic[], int x, int y, int xspeed, int yspeed) {
+//allocate space for the "object"
+	Sprite * sp = (Sprite *) malloc(sizeof(Sprite));
+	if (sp == NULL)
+		return NULL;
+// read the sprite pixmap
+	sp->map = read_xpm(pic, &(sp->width), &(sp->height));
+	if (sp->map == NULL) {
+		free(sp);
+		return NULL;
+	}
+...
+return sp;
+}
+
+void destroy_sprite(Sprite *sp) {
+if (sp == NULL)
+	return;
+free(sp->map);
+free(sp);
+sp = NULL;	// hopeless: pointer is passed by value
+			//should do this @ the caller
+}
+int animate_sprite(Sprite *sp) {
+...
+}
+/* Some useful non-visible functions */
+static int draw_sprite(Sprite *sp, char *base) {
+...
+}
+static int check_collision(Sprite *sp, char *base) {
+...
+}
+

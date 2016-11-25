0415c767dd743752bea2bc84ab291100b47850c8
up201504749 <up201504749@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-11-25 12:25:12 +0000

    askdj

--- lab5/test5.c
+++ lab5/test5.c
@@ -9,8 +9,6 @@
 #include  <machine/int86.h>
 #include <sys/mman.h>
 #include <sys/types.h>
-#include "sprite.h"
-#include "asprite.h"
 //#include "pixmap.h" // defines  pic1, pic2, etc
 
 static unsigned int counter = 0;
@@ -44,66 +42,58 @@ void *test_init(unsigned short mode, unsigned short delay) {
 	}
 	if (timer_unsubscribe_int() != 0) {
 		return NULL;
-	}
-	else {
+	} else {
 		if (vg_exit() != 0)
 			return NULL;
 
-	return result;
+		return result;
 
-}
+	}
 }
 
 int test_square(unsigned short x, unsigned short y, unsigned short size,
 		unsigned long color) {
 
-
 	int ipc_status, irq_set = keyboard_subscribe_int();
-		message msg;
-		int r;
-		unsigned long out_buf = 0;
-		int i, a;
+	message msg;
+	int r;
+	unsigned long out_buf = 0;
+	int i, a;
 
-		vg_init(0x105);
+	vg_init(0x105);
 
-
-		for(i=0; i<size; i++)
-		{
-			for(a=0; a<size; a++)
-			{
-				paintPixel(x+a, y+i, color);
-			}
-
+	for (i = 0; i < size; i++) {
+		for (a = 0; a < size; a++) {
+			paintPixel(x + a, y + i, color);
 		}
-		while (out_buf != ESC_CODE)
-		{ /* You may want to use a different condition */
-			/* Get a request message. */
 
-			if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-				printf("driver_receive failed with: %d", r);
-				continue;
-			}
-			if (is_ipc_notify(ipc_status)) { /* received notification */
-				switch (_ENDPOINT_P(msg.m_source)) {
-				case HARDWARE: /* hardware interrupt notification */
+	}
+	while (out_buf != ESC_CODE) { /* You may want to use a different condition */
+		/* Get a request message. */
 
-					if (msg.NOTIFY_ARG & irq_set) {
-						out_buf = keyboard_int_handler();
-					}
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
+
+				if (msg.NOTIFY_ARG & irq_set) {
+					out_buf = keyboard_int_handler();
 				}
 			}
 		}
-		if (keyboard_unsubscribe_int() != 0) {
-			return 1;
-		}
-			out_buf = keyboard_int_handler();
+	}
+	if (keyboard_unsubscribe_int() != 0) {
+		return 1;
+	}
+	out_buf = keyboard_int_handler();
 
-			if (vg_exit() != 0)
-				return 1;
+	if (vg_exit() != 0)
+		return 1;
 
-		return 0;
-
-
+	return 0;
 
 }
 
@@ -111,163 +101,151 @@ int test_line(unsigned short xi, unsigned short yi, unsigned short xf,
 		unsigned short yf, unsigned long color) {
 
 	int ipc_status, irq_set = keyboard_subscribe_int();
-			message msg;
-			int r;
-			unsigned long out_buf = 0;
-			int i, a;
+	message msg;
+	int r;
+	unsigned long out_buf = 0;
+	int i, a;
 
-			vg_init(0x105);
+	vg_init(0x105);
 
+	make_line(xi, xf, yi, yf, color);
 
-			make_line(xi, xf, yi, yf, color);
+	while (out_buf != ESC_CODE) { /* You may want to use a different condition */
+		/* Get a request message. */
 
-			while (out_buf != ESC_CODE)
-			{ /* You may want to use a different condition */
-				/* Get a request message. */
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
 
-				if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-					printf("driver_receive failed with: %d", r);
-					continue;
-				}
-				if (is_ipc_notify(ipc_status)) { /* received notification */
-					switch (_ENDPOINT_P(msg.m_source)) {
-					case HARDWARE: /* hardware interrupt notification */
-
-						if (msg.NOTIFY_ARG & irq_set) {
-							out_buf = keyboard_int_handler();
-						}
-					}
+				if (msg.NOTIFY_ARG & irq_set) {
+					out_buf = keyboard_int_handler();
 				}
 			}
-			if (keyboard_unsubscribe_int() != 0) {
-				return 1;
-			}
-				out_buf = keyboard_int_handler();
+		}
+	}
+	if (keyboard_unsubscribe_int() != 0) {
+		return 1;
+	}
+	out_buf = keyboard_int_handler();
 
-				if (vg_exit() != 0)
-					return 1;
+	if (vg_exit() != 0)
+		return 1;
 
-			return 0;
-
-
+	return 0;
 
 }
 
 int test_xpm(unsigned short xi, unsigned short yi, char *xpm[]) {
 
-
 	int ipc_status, irq_set = keyboard_subscribe_int();
-				message msg;
-				int r;
-				unsigned long out_buf = 0;
-				int b, a;
+	message msg;
+	int r;
+	unsigned long out_buf = 0;
+	int b, a;
+	int largura, altura;
 
-				vg_init(0x105);
+	vg_init(0x105);
 
+	paint_xpm(xi, yi, xpm);
 
-				int wd, hg;
-				char *sprite = read_xpm(xpm, &wd, &hg);
+	while (out_buf != ESC_CODE) { /* You may want to use a different condition */
+		/* Get a request message. */
 
-				for(a=0; a<hg; a++)
-				{
-					for(b=0; b<wd; b++)
-					{
-						paintPixel(xi+b, yi+a, *(sprite +a*wd+b));
-					}
-				}
-				free(sprite);
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
 
-
-				while (out_buf != ESC_CODE)
-				{ /* You may want to use a different condition */
-					/* Get a request message. */
-
-					if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-						printf("driver_receive failed with: %d", r);
-						continue;
-					}
-					if (is_ipc_notify(ipc_status)) { /* received notification */
-						switch (_ENDPOINT_P(msg.m_source)) {
-						case HARDWARE: /* hardware interrupt notification */
-
-							if (msg.NOTIFY_ARG & irq_set) {
-								out_buf = keyboard_int_handler();
-							}
-						}
-					}
-				}
-				if (keyboard_unsubscribe_int() != 0) {
-					return 1;
-				}
+				if (msg.NOTIFY_ARG & irq_set) {
 					out_buf = keyboard_int_handler();
+				}
+			}
+		}
+	}
+	if (keyboard_unsubscribe_int() != 0) {
+		return 1;
+	}
+	out_buf = keyboard_int_handler();
 
-					if (vg_exit() != 0)
-						return 1;
+	if (vg_exit() != 0)
+		return 1;
 
-				return 0;
-
+	return 0;
 
 }
 
 int test_move(unsigned short xi, unsigned short yi, char *xpm[],
 		unsigned short hor, short delta, unsigned short time) {
 
-	int ipc_status, irq_set = timer_subscribe_int();
-		message msg;
-		int r;
-		int counter;
-		int a,b;
-		if (time <= 0) {
-			printf("Time not valid!");
-			return 1;
+	int ipc_status, irq_set = keyboard_subscribe_int();
+	int irq_set2 = timer_subscribe_int();
+	message msg;
+	int r;
+	unsigned long out_buf = 0;
+	int i, a;
+	void *video_mem_val = vg_init(0x105);
+
+
+	float nr_int_total = delta/(time * 60);
+	float v= nr_int_total;
+	float nr_int_atual = 0;
+
+	while (out_buf != ESC_CODE) {
+
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+			printf("driver_receive failed with: %d", r);
+			continue;
 		}
-		vg_init(0x105);
-		int wd,hg;
-		char *st = read_xpm(xpm, &wd, &hg);
-		Sprite *sprite = create_sprite(st,xi,yi,1,1);
-		for(a=0; a<hg; a++)
-						{
-							for(b=0; b<wd; b++)
-							{
-								paintPixel(xi+b, yi+a, *(st +a*wd+b));
-							}
-						}
-						free(sprite);
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
 
+				if (msg.NOTIFY_ARG & irq_set) {
+					out_buf = keyboard_int_handler();
 
-		while (counter <= (time * 60)) { /* You may want to use a different condition */
-			/* Get a request message. */
-
-			if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-				printf("driver_receive failed with: %d", r);
-				continue;
-			}
-			if (is_ipc_notify(ipc_status)) { /* received notification */
-				switch (_ENDPOINT_P(msg.m_source)) {
-				case HARDWARE: /* hardware interrupt notification */
-
-					if (msg.NOTIFY_ARG & irq_set) {
-						timer_int_handler(&counter);
-						animate_sprite(sprite,hor,delta);
-
-					}
+						break;
 				}
+
+				if (msg.NOTIFY_ARG & irq_set2) {
+
+					if(nr_int_atual > nr_int_total)
+						break;
+
+					if (hor == 0)
+							yi += v;
+						else
+							xi += v;
+
+					nr_int_atual++;
+
+					memset(video_mem_val, 0, H_RES * V_RES * BITS_PER_PIXEL / 8);
+					paint_xpm(xi, yi, xpm);
+
+				}
+
 			}
 		}
-		if (timer_unsubscribe_int() != 0) {
-			return 0;
-		}
-		else {
-			if (vg_exit() != 0)
-				return 1;
-
-		return 0;
-
 	}
 
+	if (timer_unsubscribe_int() != 0) {
+		return 1;
+	}
+	if (keyboard_unsubscribe_int() != 0) {
+		return 1;
+	}
+	out_buf = keyboard_int_handler();
 
+	if (vg_exit() != 0)
+		return 1;
 
-
+	return 0;
 }
 
 int test_controller() {
@@ -275,4 +253,3 @@ int test_controller() {
 	/* To be completed */
 
 }
-

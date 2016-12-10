a2b195bdff07af1fa7f0076c251d5e2bd4f744ad
up201504749 <up201504749@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-12-10 23:01:21 +0000

    asd

---
+++ proj/src/objects.c
@@ -0,0 +1,25 @@
+#include "objects.h"
+
+
+Game_object *new_object(unsigned short row, unsigned short col,object_name name)
+{
+	Game_object *obj = (Game_object*)(malloc(sizeof(Game_object)));
+	obj->row = row;
+	obj->col = col;
+	obj->name = name;
+	switch(obj->name)
+	case FRUIT:
+		obj->type = FRIENDLY_TYPE;
+		obj->point_value = 2;
+		break;
+	case SNAPCHAT:
+		obj->type= FRIENDLY_TYPE;
+		obj-> point_value = 4;
+		break;
+	case OBSTACLE:
+		obj->type= DANGER_TYPE
+		obj-> point_value = 0;
+		break;
+	default:
+		break;
+}

fdf5a9399a1a047701ef3b15df0f2c6eea64f5f1
up201503344 <up201503344@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-12-11 19:42:41 +0000

    update

---
+++ proj/src/man_events.h
@@ -0,0 +1,16 @@
+#ifndef __MAN_EVENTS_H
+#define __MAN_EVENTS_H
+
+void keyboard_event_handler(unsigned long out_buf);
+void timer_event_handler(unsigned short counter);
+
+
+
+
+
+
+
+
+
+
+#endif /* __MAN_EVENTS_H */

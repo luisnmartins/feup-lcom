7d8e44c19a4af1269e647f945ddb308c107efd68
Luís Martins <luis.n.martins97@gmail.com>
2016-12-11 19:42:41 +0000

    update

---
+++ proj/src/man_events.c
@@ -0,0 +1,15 @@
+
+void keyboard_event_handler(unsigned long out_buf)
+{
+
+	if(out_buf & 0xFF00 == 0xE0)
+	{
+
+	}
+}
+
+void timer_event_handler(unsigned short counter)
+{
+	//TODO if(single_p state) {gera ecra e cobra, continue, espera pelo clique da tecla para)
+
+}

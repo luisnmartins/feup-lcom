67cd7da852b08e867024fd9ce718a602869ba493
up201504749 <CarlosFr97@users.noreply.github.com>
2016-12-11 19:48:24 +0000

    asdw

--- proj/src/man_events.c
+++ proj/src/man_events.c
@@ -1,4 +1,42 @@
 
+
+typedef enum {
+	MENU, SP, MP, MOKB ,KBC1, EXIT
+} states;
+typedef enum {
+	OPT1,OPT2,OPT3 //mais cenas
+} event;
+
+
+void check_game_status(states *st, event *ev)
+
+{
+	switch(*st){
+	case MENU:
+		if(*ev == OPT1)
+			*st = SP;
+		else if (*ev == OPT2)
+			*st = MP;
+		else if (*ev == OPT3)
+			*st = EXIT;
+		break;
+	case EXIT:
+		//sai do programa
+		break;
+	case SP:
+		//TODO
+		break;
+	case MP:
+		if(*ev == OPT1)
+			*st = KBC1;
+		else if (*ev == OPT2)
+			*st = MOKB;
+		break;
+
+	}
+
+}
+
 void keyboard_event_handler(unsigned long out_buf)
 {
 

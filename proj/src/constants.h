a61576c7c64685d88bc78488f54cda8d5693fd41
up201503344 <up201503344@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-12-15 16:14:25 +0000

    add constants.h

---
+++ proj/src/constants.h
@@ -0,0 +1,13 @@
+
+
+//-------------- GRAPHICS
+
+#define H_RES             1280
+#define V_RES		  1024
+#define BITS_PER_PIXEL	  16
+#define SCREEN_SIZE  H_RES*V_RES*BITS_PER_PIXEL/8
+
+
+// -------------- KEYB0ARD
+
+#define FIRST_BYTE_ARROWS 0xE0

871cc0b79f991727905c6a368455a5c087eb8e87
up201503344 <up201503344@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-12-29 20:57:41 +0000

    update delay nrs

--- proj/src/date.h
+++ proj/src/date.h
@@ -10,4 +10,10 @@ typedef struct
 	unsigned long year;
 }date_rtc;
 
+/**
+ *
+ * @brief Update the date struct pointer with the current hour
+ *
+ * @param date Pointer to an element of date_rtc type where the hour is setted
+ */
 void update_date(date_rtc *date);

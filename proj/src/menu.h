8e9fe95a2dc735d940b7176637de16dc0be3a3ba
up201504749 <up201504749@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-12-31 17:07:58 +0000

    asdas

--- proj/src/menu.h
+++ proj/src/menu.h
@@ -4,20 +4,76 @@
 #include "constants.h"
 
 
-
+/**
+ * @brief it fills the bmp global variable with the bitmaps loaded in graphics.c
+ * */
 void start_variables_menu();
+/**
+ * @brief it updates the x_pos_atual and y_pos_atual of the mouse
+ * @param x deltax given by the mouse packet(at end x is given the value of the current position of mouse not deltax)
+ * @param y deltay given by the mouse packet(at the end y is given the value of the current position of the mouse not deltay)
+ * */
+void update_pos_mouse(unsigned long *x, unsigned long *y);
+/**
+ * @brief draws in the screen the menu specified
+ * @param mode to know what menu is going to draw, 0 for the main menu, 1 for the multiplayer menu
+ * */
+void draw_menu(int mode);
+/**
+ * @brief draws in the screen the mouse cursor in the current position of the mouse
+ * */
+void  update_menu_mouse();
+/**
+ * @brief it changes the bmp associated with the variable body
+ * @param opt wich of the bodies is being chosen
+ * */
+void change_body(int opt);
+/**
+ * @brief it changes the bmp associated with the variable body2
+ * @para opt which of the bodies as been chosen
+ * */
+void change_body2(int opt);
+/**
+ * @brief it changes all the bmps associated with the variables of the head of snake s1
+ * @param opt which of the heads has been chosen in the choose the body menu
+ * */
+void change_head(int opt);
+/**
+ * @brief draws the menus to choose the body of the snakes
+ * @param mode variabel to know which choose menu is going to draw, 1 for the menu that can change body and head of snake s1, 1 and 2 for menus to choose only the bodies of the snakes s1 and s2 respectively
+ *
+ * */
+void draw_choose_snake(int mode);
 
-void update_pos_mouse(unsigned long *x, unsigned long *y);// pode ser metida no menu.c cuidado com as variaveis x_pos,y_pos_atual
-void draw_menu(int mode);//pode ser metida no menu.c
-void  update_menu_mouse();//pode ser metida no menu.c cuidado com os bitmaps  static
-void change_body(int opt); // pode ser metida no menu.c cuidado com as statics bitmaps
-void change_body2(int opt);//pode ser metida no menu.c cuidado com as statics bitmaps
-void change_head(int opt);// pode ser metida no menu.c
+/**
+ * @brief draws in any of the choose menus a preview snake to see how the options chosen looks
+ * @param snake_preview the snake it is going to draw and move
+ * @param counter
+ * @param body_count
+ * */
+void draw_preview_snake(Snake *snake_preview, int counter, int body_count);
 
-void draw_choose_snake(int mode); //pode ser metida no menu .c cuidado com a bitmap
-void draw_preview_snake(Snake *snake_preview, int counter, int body_count);//pode ??
-void draw_time(int hour,int min, int seconds);//pode ser metida no menu.c cuidado com as bitmaps(array de numeros)
-void draw_instructions(int mode);//pode ser metida no menu.c
-void show_points_sp(Snake *s1);//pode ser metida no menuc.
-void show_points_mp(Snake *s1, Snake *s2);//pode ser metida no menu.c
+/**
+ * @brief it draws in the menus the current time of the system
+ * @param hour the hour received by the rtc
+ * @param min the minutes received by the rtc
+ * @param seconds the seconds received by the rtc
+ * */
+void draw_time(int hour,int min, int seconds);
+/**
+ * @brief draws the game instructions of each game mode
+ * @param mode to know which game mode it is, 1 for singleplayer 2 for 2 snakes and 3 for the snake and mouse mode (0 is to draw the pause message when game is paused)
+ * */
+void draw_instructions(int mode);
+/**
+ * @brief show the end game points of the snake(1 snake modes)
+ * @param s1 snake that the points are checked
+ * */
+void show_points_sp(Snake *s1);
+/**
+ * @brief show the end game points of both players snakes (2 snake modes)
+ * @param s1 snake of player 1 points
+ * @param s2 snake of player 2 poitns
+ * */
+void show_points_mp(Snake *s1, Snake *s2);
 

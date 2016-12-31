0162d0eba08877c51b28db29fcf1294bf132d89a
up201504749 <up201504749@f85ac55d-6a5c-42ed-8920-85ce5ecb46d8>
2016-12-31 17:24:36 +0000

    adsa

--- proj/src/keyboard.h
+++ proj/src/keyboard.h
@@ -7,16 +7,37 @@
 #include "constants.h"
 
 
-
+/**
+ * @brief subscribes and enables the keyboard interrupts
+ * */
 int keyboard_subscribe_int();
+/**
+ * @brief unsubscribes the keyboard interrupts
+ * */
 int keyboard_unsubscribe_int();
-unsigned long keyboard_test_int();
+/**
+ * @brief reads and return the output buffer of the keyboard
+ * @return the output buffer of the keyboard,useful to check keys scancodes (assembly version is being used)
+ *
+ * */
 unsigned long keyboard_int_handler();
+/**
+ * @brief checks if outbuf is of the E0, if the scan code of the key is of 2 bytes
+ * @param outbuf to check
+ * @param out_buf2 to save space for the 2 bytes
+ * @return 1 if the scan code is of 2 bytes and the it hasn been handled,0 otherwise puting the full scan code in out_buff
+ * */
 int receive_buffer(unsigned long *out_buf, unsigned long *out_buf2);
+
+/**
+ * @brief issues a cmd to the keyboard buffer
+ * @param cmd argument or command that is being written
+ * @return 1 if it failed,0 otherwise
+ * */
 int issue_cmd_kbd(unsigned long cmd);
-void print_code(unsigned long *out_buf);
-void print_led(unsigned long leds_p, unsigned long flag);
-int wait_esc();
+
+//void print_code(unsigned long *out_buf);
+//void print_led(unsigned long leds_p, unsigned long flag);
 
 
 
--- proj/src/mouse.h
+++ proj/src/mouse.h
@@ -9,10 +9,26 @@
 #include <minix/com.h>
 
 
-
+/**
+ * @brief subscribes and enables the mouse interruts
+ * @return the changed hook_id or -1 if it failed
+ * */
 int mouse_subscribe_int();
+/**
+ * @brief unsubscribes mouse interrupts
+ * @return 1 if it failed,0 otherwise
+ * */
 int mouse_unsubscribe_int();
+/**
+ * @brief reads the out bufer of the mouse
+ * @return the value of the buffer or -1 if it failed
+ * */
 unsigned long mouse_int_handler();
+/**
+ * @brief issues a command to the buffer of the mouse
+ * @param cdm command or arguments issued
+ * @return 0 if succeeds 1 otherwise
+ * */
 int issue_cmd_ms(unsigned long cmd);
 int get_packets(unsigned short *counter, unsigned long *packet_mouse, unsigned long *out_buf_mouse);
 int disable_stream_mode_end();
@@ -20,7 +36,7 @@ int set_stream_mode();
 int print_packet(int size_array, unsigned long *array, long *x, long  *y, unsigned short *lb,unsigned short *rb) ;
 long compl2(long nr);
 void print_conf_byte1(unsigned long *conf_byte);
-int is_vert(short length,short length_drawn);
+//int is_vert(short length,short length_drawn);
 //int vert_line(short size,unsigned long *array)
 
 #endif /* __MOUSE_H */
